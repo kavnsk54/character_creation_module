ae40ef02006e8066f86b2756107912abf6cda2e6
Александр Костин <kavnsk@gmail.com>
2023-09-01 14:09:10 +0700

    update

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+ if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
